76e69bfbc31785a466c427aa6a333ab29f8d1d69
Igor Postnikov <igor.postnikov8@gmail.com>
2021-03-02 21:08:46 -0800

    Lecture 2

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,28 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Igor Postnikov on 3/2/21.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in emojis[pairIndex]}
+    }
+        
+    // MARK: - Access to the Model
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card){
+        model.choose(card: card)
+    }
+}
