eeed917dd973674fec004911bfc68c563e19802a
Igor Postnikov <igor.postnikov8@gmail.com>
2021-03-08 20:21:51 -0800

    Lecture 5

---
+++ Memorize/Pie.swift
@@ -0,0 +1,36 @@
+//
+//  Pie.swift
+//  Memorize
+//
+//  Created by Igor Postnikov on 3/8/21.
+//
+
+import SwiftUI
+
+struct Pie: Shape {
+    var startAngel: Angle
+    var endAngle: Angle
+    var clockwise: Bool = false
+    
+    func path(in rect: CGRect) -> Path {
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = min(rect.width, rect.height) / 2
+        let start = CGPoint(
+            x: center.x + radius * cos(CGFloat(startAngel.radians)),
+            y: center.y + radius * sin(CGFloat(startAngel.radians))
+        )
+        
+        var p = Path()
+        p.move(to: center)
+        p.addLine(to: start)
+        p.addArc(
+            center: center,
+            radius: radius,
+            startAngle: startAngel,
+            endAngle: endAngle,
+            clockwise: clockwise
+        )
+        p.addLine(to: center)
+        return p
+    }
+}
